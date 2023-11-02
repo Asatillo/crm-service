5d8a1da605534cd78e4717edc341e76f044e440f
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2023-11-02 16:46:46 +0100

    add non null annotation to use constructor

--- src/main/java/com/example/CRM/model/Customer.java
+++ src/main/java/com/example/CRM/model/Customer.java
@@ -1,25 +1,38 @@
 package com.example.CRM.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.util.Date;
 
 @Entity
 @Table(name = "customer")
+@RequiredArgsConstructor
+@NoArgsConstructor
 @Data
 public class Customer {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NonNull
     private String firstName;
+    @NonNull
     private String lastName;
+    @NonNull
     private String phoneNumber;
+    @NonNull
     private String email;
+    @NonNull
     private String address;
+    @NonNull
     private String city;
+    @NonNull
     private Date dob;
-    private Date accCreationDate;
-    @Enumerated(EnumType.ORDINAL)
+    @NonNull
+    @Enumerated(EnumType.STRING)
     private Segment segment;
-
+    @CreationTimestamp
+    private Date accCreationDate;
 }
--- src/main/java/com/example/CRM/model/Subscription.java
+++ src/main/java/com/example/CRM/model/Subscription.java
@@ -1,23 +1,45 @@
 package com.example.CRM.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Formula;
+
+import java.util.Date;
 
 @Entity
 @Table(name = "subscription")
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class Subscription {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NonNull
     private boolean is_active;
 
+    @NonNull
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
 
+    @NonNull
     @ManyToOne
     @JoinColumn(name = "plan_id")
     private Plan plan;
-// TODO: add more fields related to the specific subscription. Like start date and end date.
+
+    @NonNull
+    @CreationTimestamp
+    private Date startDate;
+
+//    TODO: Automatically calculates during the creation of the subscription. The value is calculated by adding the duration of the plan to the start date of the subscription.
+//    @NonNull
+//    @Formula("DATE_ADD(start_date, INTERVAL plan.duration DAY)")
+//    private Date endDate;
+
 }
