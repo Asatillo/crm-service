6e352984e034c6d7adf5817aaf6d6fcbe864afa7
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2023-11-08 11:12:29 +0100

    add query methods to customer repository

--- src/main/java/com/example/CRM/repository/CustomerRepository.java
+++ src/main/java/com/example/CRM/repository/CustomerRepository.java
@@ -2,8 +2,13 @@ package com.example.CRM.repository;
 
 import com.example.CRM.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    boolean existsByEmail(String email);
+
+    boolean existsByPhoneNumber(String phoneNumber);
 }
