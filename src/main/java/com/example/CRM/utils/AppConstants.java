b5a503f9c9a27bae675f834a2650cc96f97df4ac
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2023-11-08 10:12:11 +0100

    add validation functions for email and phone

--- src/main/java/com/example/CRM/utils/AppConstants.java
+++ src/main/java/com/example/CRM/utils/AppConstants.java
@@ -5,4 +5,9 @@ public class AppConstants {
     public static final String DEFAULT_PAGE_NUMBER = "0";
     public static final String DEFAULT_PAGE_SIZE = "10";
     public static final String DEFAULT_SORT_PROPERTY = "id";
+
+    public static final String REGEX_PHONE_NUMBER = "(\\+36|06)[\\s-]?[237]0[\\s-]?\\d{7}";
+
+    // TODO: optimize regex to not start with special characters
+    public static final String REGEX_EMAIL = "^[\\w\\-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
 }
--- src/main/java/com/example/CRM/utils/AppUtils.java
+++ src/main/java/com/example/CRM/utils/AppUtils.java
@@ -17,4 +17,16 @@ public class AppUtils {
             throw new CRMApiException(HttpStatus.BAD_REQUEST, "Page size must not be greater than " + AppConstants.MAX_PAGE_SIZE);
         }
     }
+
+    public static boolean isValid(String regex, String input) {
+        return input.matches(regex);
+    }
+
+    public static boolean isValidEmail(String email) {
+        return isValid(AppConstants.REGEX_EMAIL, email);
+    }
+
+    public static boolean isValidPhoneNumber(String phoneNumber) {
+        return isValid(AppConstants.REGEX_PHONE_NUMBER, phoneNumber);
+    }
 }
