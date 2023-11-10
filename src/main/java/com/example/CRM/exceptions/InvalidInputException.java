224ad48392c6be467245dacbcb5e625334e88146
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2023-11-10 14:30:23 +0100

    fix InvalidInputException

--- src/main/java/com/example/CRM/exceptions/InvalidInputException.java
+++ src/main/java/com/example/CRM/exceptions/InvalidInputException.java
@@ -1,5 +1,6 @@
 package com.example.CRM.exceptions;
 
+import com.example.CRM.payload.ApiResponse;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -7,14 +8,21 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Getter
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class InvalidInputException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    private transient ApiResponse apiResponse;
+
     private String resourceName;
     private String fieldName;
     private Object fieldValue;
 
     public InvalidInputException(String resourceName, String fieldName, Object fieldValue) {
-        super(String.format("%s with %s '%s' is invalid", resourceName, fieldName, fieldValue));
+        super();
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
+        this.apiResponse = new ApiResponse(Boolean.FALSE, String.format("%s with %s value '%s' doesn't match the pattern", resourceName, fieldName, fieldValue));
     }
+
 }
