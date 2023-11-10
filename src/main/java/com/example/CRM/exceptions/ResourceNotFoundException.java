4839476dca0c6fbf4ab2efb4f11c103bf4db843a
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2023-11-10 15:53:40 +0100

    fix ResourceNotFoundExceptiono

--- src/main/java/com/example/CRM/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/example/CRM/exceptions/ResourceNotFoundException.java
@@ -1,5 +1,6 @@
 package com.example.CRM.exceptions;
 
+import com.example.CRM.payload.ApiResponse;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -7,14 +8,17 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Getter
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
+
+    private transient ApiResponse apiResponse;
     private String resourceName;
     private String fieldName;
     private Object fieldValue;
 
     public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
-        super(String.format("%s not found with %s: '%s'", resourceName, fieldName, fieldValue));
+        super();
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
+        this.apiResponse = new ApiResponse(Boolean.FALSE, String.format("%s with %s '%s' not found", resourceName, fieldName, fieldValue));
     }
 }
