2735181ebd10ae926778bc6ec45fd2706f6bd167
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2023-11-15 10:55:07 +0100

    add payload for subscription request

---
+++ src/main/java/com/example/CRM/payload/SubscriptionRequest.java
@@ -0,0 +1,21 @@
+package com.example.CRM.payload;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Getter
+@NoArgsConstructor
+public class SubscriptionRequest {
+    @NotNull
+    private long customerId;
+    @NotNull
+    private long planId;
+    @NotNull
+    private LocalDateTime startDate;
+}
