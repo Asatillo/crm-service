fd14b4a3c0d739967b03cf04ffed0ab3025a2fe3
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2023-11-22 11:02:29 +0100

    use Services in Plans

--- src/main/java/com/example/CRM/payload/PlanRequest.java
+++ src/main/java/com/example/CRM/payload/PlanRequest.java
@@ -1,7 +1,5 @@
 package com.example.CRM.payload;
 
-import com.example.CRM.model.Service;
-import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
