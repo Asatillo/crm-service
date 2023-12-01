517a414137b1613ab1654ef1a8c80fc564a6ca92
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2023-12-01 14:48:39 +0100

    add dependency for apis to communicate

---
+++ src/main/java/com/example/CRM/feign/CRMInterface.java
@@ -0,0 +1,8 @@
+package com.example.CRM.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient("SALES-SERVICE")
+public interface CRMInterface {
+    // the methods to be used in the SALE-SERVICE microservice
+}
