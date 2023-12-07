eaa2aa11571dc7bd3b140a61ed175922556279fe
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2023-12-07 16:17:13 +0100

    add Getter annotation to BadRequestException

--- src/main/java/com/example/CRM/exceptions/BadRequestException.java
+++ src/main/java/com/example/CRM/exceptions/BadRequestException.java
@@ -1,9 +1,11 @@
 package com.example.CRM.exceptions;
 
 import com.example.CRM.payload.ApiResponse;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Getter
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestException extends RuntimeException {
 
@@ -14,7 +16,4 @@ public class BadRequestException extends RuntimeException {
         this.apiResponse = apiResponse;
     }
 
-    public ApiResponse getApiResponse() {
-        return apiResponse;
-    }
 }
