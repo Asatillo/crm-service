9583cd4e30412252ea2a09feb0a1620d4c00f111
Asat Akhmedov <asatillo.akhmedov@gmail.com>
2024-05-13 12:43:51 +0200

    remove unused constants

--- src/main/java/com/example/CRM/utils/AppConstants.java
+++ src/main/java/com/example/CRM/utils/AppConstants.java
@@ -9,21 +9,6 @@ public class AppConstants {
     public static final String DEFAULT_PAGE_SIZE = "10";
     public static final String DEFAULT_SORT_PROPERTY = "id";
 
-    public static final String REGEX_PHONE_NUMBER = "(36[237]0\\d{7})";
-
-    /*
-    * (25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}): Matches each octet, ensuring it's a valid number between 0 and 255.
-    * 25[0-5]: Matches numbers 250 to 255.
-    * 2[0-4][0-9]: Matches numbers 200 to 249.
-    * [0-1]?[0-9]{1,2}: Matches numbers 0 to 199, allowing for single or double-digit numbers.
-    * (\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})){3}: Matches the remaining three octets, separated by dots.
-    * */
-    public static final String REGEX_ROUTER_IDENTIFIER = "((25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})(\\.(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})){3})";
-
-    public static final String DEVICE_TYPES_REGEX = "MOBILE|ROUTER";
-
-    public static final String REGEX_NETWORK_IDENTIFIER = REGEX_PHONE_NUMBER + "|" + REGEX_ROUTER_IDENTIFIER;
-
     public static final List<String> CITIES_WITH_WIRED_INTERNET = List.of("Budapest");
 
     public static final Integer MAJORITY_AGE = 16;
